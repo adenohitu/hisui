0fe8cf40dd19769c23896b8b9aae3de1846e84d1
Adenohitu <49752462+jmjmri@users.noreply.github.com>
2021-04-08 17:51:07 +0900

    feat:TaskListを追加
    エディターの横に問題を選択できるタブを追加

---
+++ src/app/Slice/taskdata.ts
@@ -0,0 +1,53 @@
+//スコアデータを管理
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AppThunk, RootState } from "../store";
+
+interface taskData {
+  taskData: any;
+  //ロード中はtrue
+  load: boolean;
+}
+const initialState: taskData = {
+  taskData: [],
+  load: false,
+};
+
+export const taskDataSlice = createSlice({
+  name: "taskData",
+  initialState,
+  reducers: {
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    setdata: (state, action: PayloadAction<any>) => {
+      state.taskData = action.payload;
+    },
+    loadStart: (state) => {
+      state.load = true;
+    },
+    loadEnd: (state) => {
+      state.load = false;
+    },
+  },
+});
+export const { setdata, loadStart, loadEnd } = taskDataSlice.actions;
+export const sendGetTasklist = (): AppThunk => async (dispatch, getState) => {
+  if (getState().submissionsData.load === false) {
+    await window.api.getTasklist_on_render((arg: any) => {
+      if (arg !== "reqError") {
+        dispatch(setdata(arg));
+      } else dispatch(loadEnd);
+    });
+    window.api.getTasklist_send_render();
+    dispatch(loadStart);
+    //ipc受信部
+  } else {
+    console.log("nowloading");
+  }
+};
+
+export const selecttaskData = (state: RootState) => {
+  return state.taskData.taskData;
+};
+export const selectload = (state: RootState) => {
+  return state.taskData.load;
+};
+export default taskDataSlice.reducer;
