5c2642ae203cbc855e64253418bbcf3fe07657be
Adenohitu <49752462+jmjmri@users.noreply.github.com>
2021-04-12 17:30:26 +0900

    update:無駄なログ出力を削除

--- electron/preload.ts
+++ electron/preload.ts
@@ -98,9 +98,7 @@ contextBridge.exposeInMainWorld("api", {
   },
   getRank_on_render: (func: any) => {
     ipcRenderer.once("getRank_replay", (event, arg) => {
-      console.log(event);
       // console.log(func);
-      console.log("onnnnnnnn");
       func(arg);
     });
   },
@@ -116,7 +114,6 @@ contextBridge.exposeInMainWorld("api", {
   },
   getTotal_on_render: (func: any) => {
     ipcRenderer.once("getTotal_replay", (event, arg) => {
-      console.log(event);
       func(arg);
     });
   },
@@ -141,8 +138,6 @@ contextBridge.exposeInMainWorld("api", {
   },
   getSubmissions_on_render: (func: any) => {
     ipcRenderer.once("getSubmissionsMe_replay", (event, arg) => {
-      console.log(arg);
-      console.log("ouuuuu");
       func(arg);
     });
   },
