dbfafd783880173f7821adc096cb3ba71fae415a
Adenohitu <49752462+jmjmri@users.noreply.github.com>
2021-04-12 18:15:32 +0900

    feat:ユーザーデータをキャッシュする

--- electron/data/userdata.ts
+++ electron/data/userdata.ts
@@ -1,18 +1,27 @@
 import { Atcoder } from "./atcoder";
 import { scrapingUserData } from "./scraping/userdata";
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
 export const getUserData = async (userid: string = Atcoder.getUsername()) => {
+  const cacheData = myCache.get(`User_${userid}`);
   const userDataUrl = `https://atcoder.jp/users/${userid}?lang=ja`;
-  const data = await Atcoder.axiosInstance.get(userDataUrl, {
-    maxRedirects: 0,
-    validateStatus: function (status) {
-      return status < 500;
-    },
-  });
-
-  if (data.status === 200) {
-    const returnData = await scrapingUserData(data.data);
-    return returnData;
+  if (cacheData === undefined) {
+    const data = await Atcoder.axiosInstance.get(userDataUrl, {
+      maxRedirects: 0,
+      validateStatus: function (status) {
+        return status < 500;
+      },
+    });
+    if (data.status === 200) {
+      const returnData = await scrapingUserData(data.data);
+      myCache.set(`User_${userid}`, returnData, 3600);
+      console.log(`user:${userid}_set`);
+      return returnData;
+    } else {
+      return "reqError";
+    }
   } else {
-    return "reqError";
+    console.log(`user:${userid}_load`);
+    return cacheData;
   }
 };
