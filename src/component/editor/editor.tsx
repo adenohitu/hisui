896c5f1fa4dfb66a03ba19906a5bff0e7f2f9535
Adenohitu <49752462+jmjmri@users.noreply.github.com>
2021-04-14 16:50:47 +0900

    test:multi module editor のテスト
    StateでPathを飼えることで複数ファイルを同時に開いた状態にできる

--- src/component/editor/editor.tsx
+++ src/component/editor/editor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 // import path from "path";
 import Editor, { loader } from "@monaco-editor/react";
 // loader.config({
@@ -9,33 +9,58 @@ import Editor, { loader } from "@monaco-editor/react";
 loader.config({
   paths: { vs: "./monaco-editor/min/vs" },
 });
-const testdata = `#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int h, a;
-    cin >> h >> a;
-    int ans;
-    ans = 0;
-    while (h > 0) {
-        h -= a;
-        ans++;
-    }
-    cout << ans << endl;
-    return 0;
-}
-`;
+const files: any = {
+  "script.js": {
+    name: "script.js",
+    language: "javascript",
+    value: "script.js",
+  },
+  "style.css": {
+    name: "style.css",
+    language: "css",
+    value: "style.css",
+  },
+  "index.html": {
+    name: "index.html",
+    language: "html",
+    value: "index.html",
+  },
+};
 export function MainEditor() {
+  const [fileName, setFileName]: any = useState("script.js");
+  const file = files[fileName];
   useEffect(() => {
     console.log();
   }, []);
   return (
-    <Editor
-      height="100%" // By default, it fully fits with its parent
-      theme={"light"}
-      language={"cpp"}
-      value={testdata}
-      loading={"Loading..."}
-    />
+    <>
+      <div>
+        <button
+          disabled={fileName === "script.js"}
+          onClick={() => setFileName("script.js")}
+        >
+          script.js
+        </button>
+        <button
+          disabled={fileName === "style.css"}
+          onClick={() => setFileName("style.css")}
+        >
+          style.css
+        </button>
+        <button
+          disabled={fileName === "index.html"}
+          onClick={() => setFileName("index.html")}
+        >
+          index.html
+        </button>
+      </div>
+      <Editor
+        height="100%"
+        loading={"loading..."}
+        path={file.name}
+        defaultLanguage={file.language}
+        defaultValue={file.value}
+      />
+    </>
   );
 }
