7962f2d42cb422f9a448e7ec70e340cb9d727479
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2021-06-16 21:50:04 +0900

    archive:rename

--- archive/window_editor/notest.jsx
+++ archive/window_editor/notest.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import _ from "lodash";
 import { Responsive, WidthProvider } from "react-grid-layout";
-import "./test";
+import "./notest";
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 class ToolBoxItem extends React.Component {
