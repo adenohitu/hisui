430c87e6cb2022ada0b0896af858e16f80cd0ff9
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2021-06-16 17:10:22 +0900

    comment:コメントを追加

--- electron/event/event.ts
+++ electron/event/event.ts
@@ -4,25 +4,6 @@ import { hisuiEventtType } from "../@types/hisui";
 /**
  * Hisuiのイベント
  * EventEmitterを継承
+ * eventは　hisui.d.tsに記入
  */
 export const hisuiEvent: hisuiEventtType = new EventEmitter();
-/**
- * event一覧
- * コンテスト中に関するイベント
- * contest-start
- * デフォルトのコンテストが開始時刻になると一回呼び出される
- * contest-end
- * デフォルトのコンテストが終了時刻になると一回呼び出される
- *
- * 認証処理に関するイベント
- * login
- * ログインが成功した時に呼び出される
- * logout
- * ログアウトが実行された時に呼び出される
- *
- * Viewに関するイベント
- * hisuiEvent.emit('view-main-top', viewName);
- * MainWindowで一番上のViewが変更された時に呼び出される
- *
- *
- */
