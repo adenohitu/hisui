49f7b5eabed6bba58e46f4a0424f2215da902f2d
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2021-06-23 19:05:09 +0900

    feat:イベントを追加

--- electron/@types/hisui.d.ts
+++ electron/@types/hisui.d.ts
@@ -32,6 +32,13 @@ export declare class hisuiEventtType extends EventEmitter {
    */
   on(event: "contest-end", listener: () => void): this;
 
+  emit(event: "DefaultContestID-change", args: string): boolean;
+  /**
+   * デフォルトのコンテストが変更されると実行
+   * 変更後のコンテストIDを返す
+   */
+  on(event: "DefaultContestID-change", listener: (args: string) => void): this;
+
   // --MainWindowに関するイベント--
 
   emit(event: "view-main-top", args: string): boolean;
