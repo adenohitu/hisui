4202c4974ef4418fd1020eaa3e1d4eccef6553f4
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2021-09-25 13:33:15 +0900

    feat:testを追加

---
+++ src_main/data/__test__/samplecase.test.ts
@@ -0,0 +1,16 @@
+import fs from "fs/promises";
+import { scrapingSampleCase } from "../scraping/samplecase";
+async function runTasklist() {
+  const get = await fs.readFile(__dirname + "/test.html", "utf-8");
+  const returndata = scrapingSampleCase(get);
+  return returndata;
+}
+const ans = [
+  { name: "1", case: "6\n123 223 123 523 200 2000\n" },
+  { name: "2", case: "5\n1 2 3 4 5\n" },
+  { name: "3", case: "8\n199 100 200 400 300 500 600 200\n" },
+];
+test("scraping SampleCase", async () => {
+  const data = await runTasklist();
+  expect(data).toStrictEqual(ans);
+});
