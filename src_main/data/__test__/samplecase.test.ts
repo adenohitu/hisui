bbc866aab7e58f56385492b89027df41844b326a
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2021-10-02 12:07:35 +0900

    test:テストを更新

--- src_main/data/__test__/samplecase.test.ts
+++ src_main/data/__test__/samplecase.test.ts
@@ -6,9 +6,9 @@ async function runTasklist() {
   return returndata;
 }
 const ans = [
-  { name: "1", case: "6\n123 223 123 523 200 2000\n" },
-  { name: "2", case: "5\n1 2 3 4 5\n" },
-  { name: "3", case: "8\n199 100 200 400 300 500 600 200\n" },
+  { name: "1", case: "6\n123 223 123 523 200 2000\n", answer: "4\n" },
+  { name: "2", case: "5\n1 2 3 4 5\n", answer: "0\n" },
+  { name: "3", case: "8\n199 100 200 400 300 500 600 200\n", answer: "9\n" },
 ];
 test("scraping SampleCase", async () => {
   const data = await runTasklist();
