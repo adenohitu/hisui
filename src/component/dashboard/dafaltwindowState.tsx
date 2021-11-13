62bb5cbcdefb12f03bd8dbe954c3266d66352277
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2021-11-13 17:55:36 +0900

    refactor:デフォルトの設定を変更

--- src/component/dashboard/dafaltwindowState.tsx
+++ src/component/dashboard/dafaltwindowState.tsx
@@ -1,48 +1,17 @@
 import { MosaicNode } from "react-mosaic-component";
 
 export const dashboadWindowState: MosaicNode<string> = {
-  direction: "row",
   first: {
     direction: "column",
-    first: { direction: "row", first: "time", second: "rank" },
-    second: "submission",
-    splitPercentage: 23,
+    first: {
+      direction: "row",
+      first: "time",
+      second: "rank",
+    },
+    second: "score",
+    splitPercentage: 24,
   },
-  second: {
-    direction: "column",
-    first: "score",
-    second: "totaltable",
-    splitPercentage: 75,
-  },
-};
-
-// export const dashboadWindowState: MosaicNode<string> | null = {
-//   first: {
-//     first: {
-//       direction: "row",
-//       first: "time",
-//       second: "rank",
-//     },
-//     second: "score",
-//     direction: "column",
-//     splitPercentage: 22,
-//   },
-//   second: "submission",
-//   direction: "row",
-// };
-
-export const editorWindowState: MosaicNode<string> | null = {
-  direction: "row",
-  first: {
-    direction: "row",
-    first: "time",
-    second: "editor",
-    splitPercentage: 23,
-  },
-  second: {
-    direction: "column",
-    first: "score",
-    second: "rank",
-  },
-  splitPercentage: 76,
+  second: "totaltable",
+  direction: "column",
+  splitPercentage: 80,
 };
