d1aff37e2b93c83734998867a45f93a8014a9350
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2021-12-10 16:59:49 +0900

    feat:Chipのスタイルを変更

--- src/component/editor/tool/selecter/select-tree.tsx
+++ src/component/editor/tool/selecter/select-tree.tsx
@@ -4,16 +4,10 @@ import Box from "@mui/material/Box";
 import TreeView from "@mui/lab/TreeView";
 import TreeItem, { TreeItemProps, treeItemClasses } from "@mui/lab/TreeItem";
 import Typography from "@mui/material/Typography";
-import MailIcon from "@mui/icons-material/Mail";
-import DeleteIcon from "@mui/icons-material/Delete";
-import Label from "@mui/icons-material/Label";
-import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
-import InfoIcon from "@mui/icons-material/Info";
-import ForumIcon from "@mui/icons-material/Forum";
-import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
-import SvgIcon, { SvgIconProps } from "@mui/material/SvgIcon";
+import { SvgIconProps } from "@mui/material/SvgIcon";
+import AssignmentIcon from "@mui/icons-material/Assignment";
 import { Chip } from "@mui/material";
 
 declare module "react" {
@@ -39,6 +33,11 @@ const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({
     borderBottomRightRadius: theme.spacing(2),
     paddingRight: theme.spacing(1),
     fontWeight: theme.typography.fontWeightMedium,
+    padding: "0 4px",
+    ".MuiTreeItem-iconContainer": {
+      width: "0",
+      marginRight: 0,
+    },
     "&.Mui-expanded": {
       fontWeight: theme.typography.fontWeightRegular,
     },
@@ -75,8 +74,16 @@ function StyledTreeItem(props: StyledTreeItemProps) {
     <StyledTreeItemRoot
       label={
         <Box sx={{ display: "flex", alignItems: "center", p: 0.5, pr: 0 }}>
+          {/* <Box component={LabelIcon} color="inherit" sx={{ mr: 1 }} /> */}
           <Chip
-            sx={{ mr: 1, bgcolor: "green", color: "white" }}
+            sx={{
+              mr: 1,
+              bgcolor: "green",
+              color: "white",
+              height: "15px",
+              fontSize: "13px",
+              paddingTop: "2px",
+            }}
             size="small"
             label="AC"
           />
@@ -107,48 +114,46 @@ export function TaskSelectTree() {
       defaultExpanded={["3"]}
       defaultCollapseIcon={<ArrowDropDownIcon />}
       defaultExpandIcon={<ArrowRightIcon />}
-      defaultEndIcon={<div style={{ width: 24 }} />}
-      sx={{ height: "100%", flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
+      sx={{ height: "100%", flexGrow: 1, overflowY: "auto" }}
     >
-      <StyledTreeItem nodeId="1" labelText="All Mail" labelIcon={MailIcon} />
-      <StyledTreeItem nodeId="2" labelText="Trash" labelIcon={DeleteIcon} />
-      <StyledTreeItem nodeId="3" labelText="Categories" labelIcon={Label}>
-        <StyledTreeItem
-          nodeId="5"
-          labelText="Social"
-          labelIcon={SupervisorAccountIcon}
-          labelInfo="90"
-          color="#1a73e8"
-          bgColor="#e8f0fe"
-        />
-        <StyledTreeItem
-          nodeId="6"
-          labelText="Updates"
-          labelIcon={InfoIcon}
-          labelInfo="2,294"
-          color="#e3742f"
-          bgColor="#fcefe3"
-        />
-        <StyledTreeItem
-          nodeId="7"
-          labelText="Forums"
-          labelIcon={ForumIcon}
-          labelInfo="3,566"
-          color="#a250f5"
-          bgColor="#f3e8fd"
-        />
+      <StyledTreeItem
+        nodeId="1"
+        labelText="A-asnias"
+        labelIcon={AssignmentIcon}
+      />
+      <StyledTreeItem
+        disabled
+        nodeId="3"
+        labelText="A-asnias"
+        labelIcon={AssignmentIcon}
+      />
+      <StyledTreeItem
+        nodeId="4"
+        labelText="B-ashuags"
+        labelIcon={AssignmentIcon}
+      />
+      <StyledTreeItem nodeId="7" labelText="History" labelIcon={AssignmentIcon}>
         <StyledTreeItem
           nodeId="8"
-          labelText="Promotions"
-          labelIcon={LocalOfferIcon}
-          labelInfo="733"
-          color="#3c8039"
-          bgColor="#e6f4ea"
+          labelText="History"
+          labelIcon={AssignmentIcon}
         />
       </StyledTreeItem>
-      <StyledTreeItem nodeId="9" labelText="History" labelIcon={Label} />
-      <StyledTreeItem nodeId="10" labelText="History" labelIcon={Label} />
-      <StyledTreeItem nodeId="11" labelText="History" labelIcon={Label} />
+      <StyledTreeItem
+        nodeId="9"
+        labelText="History"
+        labelIcon={AssignmentIcon}
+      />
+      <StyledTreeItem
+        nodeId="10"
+        labelText="History"
+        labelIcon={AssignmentIcon}
+      />
+      <StyledTreeItem
+        nodeId="11"
+        labelText="History"
+        labelIcon={AssignmentIcon}
+      />
     </TreeView>
   );
 }
