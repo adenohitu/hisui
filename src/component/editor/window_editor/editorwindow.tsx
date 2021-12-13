5e58a9b3dacc872734302aa277bd7c24ac2e2157
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2021-12-13 23:35:08 +0900

    feat:monacoのTitleにコンテスト名が表示されるように変更

--- src/component/editor/window_editor/editorwindow.tsx
+++ src/component/editor/window_editor/editorwindow.tsx
@@ -10,6 +10,7 @@ import {
   submission,
 } from "./default";
 import { TITLE_ELEMENT } from "./default";
+import { useAppStatus } from "../tool/statusbar/status-hooks";
 
 export let focusEditor: () => void | undefined;
 export let focuscodeTest: () => void | undefined;
@@ -34,13 +35,18 @@ export const Editorwindow = () => {
   const onRelease = (windowState: MosaicNode<editorWindowMosaicKey> | null) => {
     // setState(windowState);
   };
+  const appstatusHooks = useAppStatus();
   return (
     <>
       <Mosaic<editorWindowMosaicKey>
         renderTile={(id, path) => (
           <MosaicWindow<editorWindowMosaicKey>
             path={path}
-            title={TITLE_ELEMENT[id].name}
+            title={
+              (TITLE_ELEMENT[id].name === "code" &&
+                `${appstatusHooks.contestName}-${appstatusHooks.taskname}`) ||
+              TITLE_ELEMENT[id].name
+            }
             toolbarControls={TITLE_ELEMENT[id].toolbarControls}
             className="table-window"
           >
