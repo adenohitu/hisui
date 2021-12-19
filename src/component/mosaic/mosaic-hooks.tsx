7722f0a24e63f55ee4fb603e69c73c6856914b36
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2021-12-19 15:47:46 +0900

    feat:mosaicの状態を保存するHooksを作成

---
+++ src/component/mosaic/mosaic-hooks.tsx
@@ -0,0 +1,130 @@
+import { dropRight } from "lodash";
+import { useEffect, useState } from "react";
+import {
+  MosaicNode,
+  Corner,
+  getPathToCorner,
+  getNodeAtPath,
+  MosaicParent,
+  MosaicDirection,
+  getOtherDirection,
+  updateTree,
+} from "react-mosaic-component";
+import { mosaicStateFormat } from "../../../src_main/save/utility/renderState";
+import { ipcRendererManager } from "../../ipc";
+
+interface monacoElement {
+  [viewId: string]: {
+    name: string;
+    component: JSX.Element;
+    toolbarControls:
+      | (React.ReactChild | React.ReactFragment | React.ReactPortal)[]
+      | undefined;
+  };
+}
+/**
+ * MOna
+ * @param mosaicWindowID
+ * 状態を保存するために必要な固有のID
+ * @param TITLE_ELEMENT
+ */
+export const useMosaicState = (
+  mosaicWindowID: string,
+  TITLE_ELEMENT: monacoElement,
+  defaultWindowState: MosaicNode<string>
+) => {
+  const [windowState, setState] = useState<MosaicNode<string> | null>(
+    defaultWindowState
+  );
+  function saveStoreState(value: MosaicNode<string> | null) {
+    console.log("Saved:mosaic");
+    ipcRendererManager.invoke("SAVE_MOSAIC_WINDOW_STATE", {
+      mosaicWindowID,
+      elementViewIdList: Object.keys(TITLE_ELEMENT),
+      saveState: value,
+    });
+  }
+  // setupと初期化信号の受け取り
+  useEffect(() => {
+    const setup = () => {
+      console.log(`mosaicHookSetup${mosaicWindowID}`);
+      ipcRendererManager
+        .invoke("GET_MOSAIC_WINDOW_STATE", mosaicWindowID)
+        .then((result: mosaicStateFormat) => {
+          if (result) {
+            setState(result.saveState);
+          }
+        });
+    };
+    setup();
+  }, [mosaicWindowID, defaultWindowState]);
+  /**
+   * 削除されたMosaicWindowをもう一度表示させる
+   */
+  // function addWindow(windowName: string) {}
+  const reRenderWindow = (windowName: string) => {
+    let currentNode = windowState;
+    if (currentNode) {
+      const path = getPathToCorner(currentNode, Corner.TOP_RIGHT);
+      const parent = getNodeAtPath(
+        currentNode,
+        dropRight(path)
+      ) as MosaicParent<string>;
+      const destination = getNodeAtPath(
+        currentNode,
+        path
+      ) as MosaicNode<string>;
+      const direction: MosaicDirection = parent
+        ? getOtherDirection(parent.direction)
+        : "row";
+
+      let first: MosaicNode<string>;
+      let second: MosaicNode<string>;
+      if (direction === "row") {
+        first = destination;
+        second = windowName;
+      } else {
+        first = windowName;
+        second = destination;
+      }
+
+      currentNode = updateTree(currentNode, [
+        {
+          path,
+          spec: {
+            $set: {
+              direction,
+              first,
+              second,
+            },
+          },
+        },
+      ]);
+    } else {
+      currentNode = windowName;
+    }
+
+    setState(currentNode);
+    saveStoreState(currentNode);
+  };
+
+  const onChange = (_windowState: MosaicNode<string> | null) => {
+    // console.log(windowState);
+    setState(_windowState);
+  };
+  const onRelease = (_windowState: MosaicNode<string> | null) => {
+    setState(_windowState);
+    saveStoreState(_windowState);
+  };
+  const resetDefaultState = () => {
+    setState(defaultWindowState);
+    saveStoreState(defaultWindowState);
+  };
+  return {
+    windowState,
+    reRenderWindow,
+    onChange,
+    onRelease,
+    resetDefaultState,
+  };
+};
--- src_main/save/utility/renderState.ts
+++ src_main/save/utility/renderState.ts
@@ -1,8 +1,29 @@
 //ウィンドウの状態に関するvalueを保存
 import { store } from "../save";
-export const getWindowState = () => {
-  return store.get("WindowState", null);
-};
-export const saveWindowState = (value: any) => {
-  store.set("WindowState", value);
-};
+
+function makeSaveMosaicKey(mosaicWindowID: string) {
+  return `WindowState_${mosaicWindowID}`;
+}
+
+export interface mosaicStateFormat {
+  mosaicWindowID: string;
+  elementViewIdList: string[];
+  saveState: any;
+}
+/**
+ * storeにMosaicの状態を保存
+ */
+export function saveMosaicState(arg: mosaicStateFormat): void {
+  const saveKey = makeSaveMosaicKey(arg.mosaicWindowID);
+  return store.set(saveKey, arg);
+}
+/**
+ * storeからMosaicの状態を取得
+ */
+export function loadMosaicState(
+  mosaicWindowID: string
+): mosaicStateFormat | null {
+  const saveKey = makeSaveMosaicKey(mosaicWindowID);
+  const storeData: any = store.get(saveKey, null);
+  return storeData;
+}
