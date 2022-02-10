15ae1a3746b0e440335c66119eeb7430ebc14066
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2022-02-10 16:44:31 +0900

    feat:prettierを導入

--- src/component/rank/rank.tsx
+++ src/component/rank/rank.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import makeStyles from '@mui/styles/makeStyles';
+import makeStyles from "@mui/styles/makeStyles";
 import Card from "@mui/material/Card";
 // import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
