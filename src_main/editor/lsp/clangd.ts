739b09781f9a9e0d440a002decdebf9127a0f20b
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2022-05-03 21:56:39 +0900

    feat:Clangdを追加

--- src_main/editor/lsp/clangd.ts
+++ src_main/editor/lsp/clangd.ts
@@ -2,8 +2,13 @@ import { prepare, installLatest } from "@clangd/install";
 import { app } from "electron";
 import { mkdir, writeFile } from "fs/promises";
 import { join, basename } from "path";
-import { languageServers } from "./lsp-server";
+import { languageServers } from "./pyright";
 import { stdcTemp } from "./stdc";
+import * as rpc from "vscode-ws-jsonrpc";
+import { Message } from "vscode-jsonrpc";
+import { spawn } from "child_process";
+import { ipcMainManager } from "../../ipc/ipc";
+import { monacoSettingApi } from "../monaco";
 
 class clangdUI {
   clangdPath: string;
@@ -100,5 +105,47 @@ export async function setupClangd(): Promise<languageServers> {
     } else {
       return {};
     }
+  }
+}
+export async function setupLSP_Clangd() {
+  const getClangdInfo = await setupClangd();
+  const setArgs = [getClangdInfo["cpp"].command[1]];
+  const lsProcess = spawn(getClangdInfo["cpp"].command[0], setArgs);
+
+  // choose a unique channel name, e.g. by using the PID
+  const ipcChannel = "ls_" + lsProcess.pid;
+
+  // create reader/writer for I/O streams
+  const reader = new rpc.StreamMessageReader(lsProcess.stdout);
+  const writer = new rpc.StreamMessageWriter(lsProcess.stdin);
+
+  // forward everything from process's stdout to the mainWindow's renderer process
+  reader.listen((msg) => {
+    ipcMainManager.send("LSP_SEND", ipcChannel, msg);
+  });
+  // listen to incoming messages and forward them to the language server process
+
+  ipcMainManager.on("LSP_ON", (event: any, pid: string, msg: Message) => {
+    if (pid === ipcChannel) {
+      writer.write(msg);
+    }
+  });
+  function sendReadySignal() {
+    ipcMainManager.send(
+      "LSP_READY",
+      {
+        id: "cpp",
+        extensions: [".cpp"],
+        aliases: ["C++", "cpp"],
+      },
+      ipcChannel
+    );
+  }
+  if (monacoSettingApi.isMonacoReady) {
+    sendReadySignal();
+  } else {
+    ipcMainManager.on("MONACO_READY", () => {
+      sendReadySignal();
+    });
   }
 }
