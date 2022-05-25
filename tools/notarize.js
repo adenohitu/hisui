64d03aa50525bc8960d38110d51da1647ed0ad4d
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2022-05-25 23:10:22 +0900

    feat:MacCodesign and notarize

---
+++ tools/notarize.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const { notarize } = require("electron-notarize");
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== "darwin") {
+    return;
+  }
+  // eslint-disable-next-line eqeqeq
+  if (process.env.CSC_IDENTITY_AUTO_DISCOVERY == "false") {
+    console.log("skip notarize");
+    return;
+  }
+  const appName = context.packager.appInfo.productFilename;
+  console.log("start notarize");
+  return await notarize({
+    appBundleId: "com.adenohitu.hisui",
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+    ascProvider: process.env.ASC_PROVIDER,
+  });
+};
