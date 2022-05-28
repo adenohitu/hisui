db74c92112e4585c70b50b804cc90becbafd4b65
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2022-05-28 18:54:03 +0900

    github:edit workflow setting
    github:edit workflow setting

--- tools/notarize.js
+++ tools/notarize.js
@@ -1,6 +1,5 @@
-require("dotenv").config();
 const { notarize } = require("electron-notarize");
-
+require("dotenv").config();
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
   if (electronPlatformName !== "darwin") {
@@ -8,16 +7,19 @@ exports.default = async function notarizing(context) {
   }
   // eslint-disable-next-line eqeqeq
   if (process.env.CSC_IDENTITY_AUTO_DISCOVERY == "false") {
-    console.log("skip notarize");
+    console.log("skip Appcation notarize");
     return;
   }
   const appName = context.packager.appInfo.productFilename;
-  console.log("start notarize");
+  const appleApiKey = process.env.APIKEYID;
+  if (!appleApiKey) throw new Error("no appleApiKey found");
+  const appleApiIssuer = process.env.APIISSUER;
+  if (!appleApiIssuer) throw new Error("no appleApiIssuer found");
+  console.log("start Appcation notarize");
   return await notarize({
     appBundleId: "com.adenohitu.hisui",
     appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-    ascProvider: process.env.ASC_PROVIDER,
+    appleApiKey,
+    appleApiIssuer,
   });
 };
