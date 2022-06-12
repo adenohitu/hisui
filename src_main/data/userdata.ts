de04b428fd23544b35f733314692ffe59609a2ed
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2022-06-12 10:53:09 +0900

    refactor:node-cacheを廃止

--- src_main/data/userdata.ts
+++ src_main/data/userdata.ts
@@ -1,8 +1,8 @@
 import { logger } from "../tool/logger/logger";
 import { Atcoder } from "./atcoder";
 import { scrapingUserData, UserData } from "./scraping/userdata";
-const NodeCache = require("node-cache");
-const myCache = new NodeCache();
+
+let cacheData: any;
 
 /**
  * ユーザーデータを取得
@@ -11,7 +11,6 @@ export async function getUserData(
   userScreenName: string | undefined = Atcoder.getUsername()
 ): Promise<"reqError" | "notSetUser" | UserData> {
   if (userScreenName !== undefined) {
-    const cacheData = myCache.get(`User_${userScreenName}`);
     const userDataUrl = `https://atcoder.jp/users/${userScreenName}?lang=ja`;
     if (cacheData === undefined) {
       const data = await Atcoder.axiosInstance.get(userDataUrl, {
@@ -22,7 +21,10 @@ export async function getUserData(
       });
       if (data.status === 200) {
         const returnData = await scrapingUserData(data.data);
-        myCache.set(`User_${userScreenName}`, returnData, 3600);
+        cacheData = returnData;
+        setInterval(() => {
+          cacheData = undefined;
+        }, 360000);
         logger.info(`GetUserStatus:${userScreenName}`, "UserDataFn");
         return returnData;
       } else {
