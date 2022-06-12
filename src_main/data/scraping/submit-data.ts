393c5c5830b9ac9de4acec4a43b006e0d5bd8c2c
Adenohitu <49752462+adenohitu@users.noreply.github.com>
2022-06-12 14:31:47 +0900

    feat:提出状況を表示するsnackbarを表示

--- src_main/data/scraping/submit-data.ts
+++ src_main/data/scraping/submit-data.ts
@@ -1,12 +1,18 @@
+import { submissionData } from "./submissions";
+
 export interface submitStatus {
+  submissionData: submissionData;
   status: string;
   labelColor: "success" | "warning" | "default" | string;
   Interval?: number;
 }
-export async function scraping_submitData(data: {
-  Html: string;
-  Interval?: number | undefined;
-}): Promise<submitStatus> {
+export async function scraping_submitData(
+  data: {
+    Html: string;
+    Interval?: number | undefined;
+  },
+  submissionData: submissionData
+): Promise<submitStatus> {
   const status = data.Html.slice(0, data.Html.search("</span>")).slice(
     data.Html.search(">") + 1
   );
@@ -14,5 +20,10 @@ export async function scraping_submitData(data: {
     data.Html.search("label-") + 6,
     data.Html.search("' data-toggle")
   );
-  return { status, labelColor, Interval: data.Interval };
+  return {
+    submissionData: submissionData,
+    status,
+    labelColor,
+    Interval: data.Interval,
+  };
 }
